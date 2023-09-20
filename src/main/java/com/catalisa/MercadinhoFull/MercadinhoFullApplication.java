8964103688dd5c1f20ac32382efc185bb8507dad
Fábio Luiz Sousa Vaz <fabio.vaz@zup.com.br>
2023-09-20 15:18:25 -0300

    :tada: Primeiros arquivos

---
+++ src/main/java/com/catalisa/MercadinhoFull/MercadinhoFullApplication.java
@@ -0,0 +1,13 @@
+package com.catalisa.MercadinhoFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadinhoFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadinhoFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalisa/MercadinhoFull/MercadinhoFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalisa.MercadinhoFull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadinhoFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
