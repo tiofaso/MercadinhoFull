1e6e7c50bf71ef953213b49cc3e52bd685fb0a38
Fábio Luiz Sousa Vaz <fabio.vaz@zup.com.br>
2023-09-20 21:54:21 -0300

    :wrench: base de dados inicial

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into tb_produto(nome_produto,preco_produto,quantidade_produto,descricao_produto,status_produto) values('Kit PLA 5 Cores','15','10','Kit com 5 cores de PLA 10M a unidade',true);
+insert into tb_produto(nome_produto,preco_produto,quantidade_produto,descricao_produto,status_produto) values('Kit PLA 10 Cores','30','15','Kit com 10 cores de PLA 10M a unidade',true);
+insert into tb_produto(nome_produto,preco_produto,quantidade_produto,descricao_produto,status_produto) values('Kit PLA 20 Cores','45','30','Kit com 20 cores de PLA 10M a unidade',true);
+insert into tb_produto(nome_produto,preco_produto,quantidade_produto,descricao_produto,status_produto) values('Kit PLA 30 Cores','55','35','Kit com 30 cores de PLA 10M a unidade',true);
